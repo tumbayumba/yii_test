96bbfd9ae52ae950123337231321b990acc1bcbd
AREON\RospopovS <Ser-19@ukr.net>
2018-09-06 17:31:36 +0300

    add country

--- config/web.php
+++ config/web.php
@@ -43,14 +43,16 @@ $config = [
             ],
         ],
         'db' => $db,
-        /*
+        
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
+                '' => 'site/index',                                
+                '<controller:\w+>/<action:\w+>/' => '<controller>/<action>',
             ],
         ],
-        */
+        
     ],
     'params' => $params,
 ];
